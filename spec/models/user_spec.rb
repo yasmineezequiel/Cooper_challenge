c4ed38a3671322d06ed2c3b4556756ae36c0ad6b
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-10-16 10:52:31 +0200

    Adds factory_bot spec and database table for user.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'should have valid Factory' do
+    expect(create(:user)).to be_valid
+  end
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :provider }
+    it { is_expected.to have_db_column :uid }
+    it { is_expected.to have_db_column :encrypted_password }
+    it { is_expected.to have_db_column :reset_password_token }
+    it { is_expected.to have_db_column :reset_password_sent_at }
+    it { is_expected.to have_db_column :remember_created_at }
+    it { is_expected.to have_db_column :sign_in_count }
+    it { is_expected.to have_db_column :current_sign_in_at }
+    it { is_expected.to have_db_column :last_sign_in_at }
+    it { is_expected.to have_db_column :current_sign_in_ip }
+    it { is_expected.to have_db_column :last_sign_in_ip }
+    it { is_expected.to have_db_column :confirmation_token }
+    it { is_expected.to have_db_column :confirmed_at }
+    it { is_expected.to have_db_column :confirmation_sent_at }
+    it { is_expected.to have_db_column :unconfirmed_email }
+    it { is_expected.to have_db_column :nickname }
+    it { is_expected.to have_db_column :image }
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :tokens }
+    it { is_expected.to have_db_column :created_at }
+    it { is_expected.to have_db_column :updated_at }
+  end
+end
