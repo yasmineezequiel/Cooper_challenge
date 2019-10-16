fe65707921d6002dbad37b4a3291aae5d62a8edd
jonas <jonas.masharqa@gmail.com>
2019-10-16 16:21:31 +0200

    Adds authentication in perfdata controller, create action, params whitelist and merge to user.

--- app/controllers/api/v1/performance_data_controller.rb
+++ app/controllers/api/v1/performance_data_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::PerformanceDataController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
+  def create
+    @data = PerformanceData.new(performance_data_params.merge(user: current_api_v1_user))
+    if @data.save
+      render json: { message: 'all good' }
+    else
+      render json: { error: @data.errors.full_messages }
+    end
+  end
+
+  private
+
+  def performance_data_params
+    params.require(:performance_data).permit!
+  end
 end
