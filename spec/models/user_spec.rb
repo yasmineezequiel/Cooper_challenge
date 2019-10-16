fafdc1b8d74d275f7c228653c67998610b351e3c
jonas <jonas.masharqa@gmail.com>
2019-10-16 11:02:24 +0200

    Adds user specs for validating email and sad and happy paths specs for emails.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,4 +29,25 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column :created_at }
     it { is_expected.to have_db_column :updated_at }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_confirmation_of(:password) }
+
+    context 'should not have an invalid email address' do
+      emails = ['asdf@ ds.com', '@example.com', 'test me @yahoo.com', 'asdf@example', 'ddd@.d. .d', 'ddd@.d']
+
+      emails.each do |email|
+        it { is_expected.not_to allow_value(email).for(:email) }
+      end
+    end
+
+    context 'should have a valid email address' do
+      emails = ['asdf@ds.com', 'hello@example.uk', 'test1234@yahoo.si', 'asdf@example.eu']
+
+      emails.each do |email|
+        it { is_expected.to allow_value(email).for(:email) }
+      end
+    end
+  end
 end
